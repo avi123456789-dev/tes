92f365ae042a4b74d5051ab63600b03a2b1bfd3f
avi123456789-dev <51450427+avi123456789-dev@users.noreply.github.com>
2019-06-11 11:17:55 +0530

    Add files via upload

--- script2.js
+++ script2.js
@@ -1,17 +1,43 @@
 var db = firebase.firestore();
-
+var q;
+var files_Array;
 function storeData(){
-     db.collection("java").add({
-    first: "Adsdadadsada",
-    last: "Lovelace",
-    born: 1815
     
+    q = document.getElementById("q_no").value;
+//    alert(q);
+    
+   db.collection("java").doc(q).set({
+    files : "newFile.txt"
 })
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
+.then(function() {
+    console.log("Document successfully written!");
 })
 .catch(function(error) {
-    console.error("Error adding document: ", error);
+    console.error("Error writing document: ", error);
 });
 
+
+}
+
+function getData(){
+    q = document.getElementById("q_no").value;
+    var docRef = db.collection("java").doc(q);
+
+docRef.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("Document data:", doc.data().files);
+        files_Array = doc.data().files;
+        document.getElementById("files_list").innerHTML += '<a href = "code/' + files_Array + '">link</a>';
+        
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document:", error);
+});
+        
+    
+    
+    
 }
